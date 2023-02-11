789afdccfe8c2840acfb641d4e066c2e90ea0925
Mohamed-320 <muhmmadabdelaziz97@gmail.com>
2023-02-11 15:26:52 +0200

    Updated class

--- OneClassSVM.py
+++ OneClassSVM.py
@@ -1,12 +1,11 @@
 import csv
 
-from numpy import genfromtxt
+from numpy import genfromtxt, quantile
 from sklearn.svm import OneClassSVM
 
 TrainData = genfromtxt(
     r"H:\zizo-thesis\upper-limb-motor-functions-data-preprocessing-evaluation\DataPreProcessing\ResampledData.csv",
-    skip_header=1,
-    delimiter=',')
+    skip_header=1, delimiter=',')
 TestData = genfromtxt(
     r"H:\zizo-thesis\upper-limb-motor-functions-data-preprocessing-evaluation\DataPreProcessing\FeaturesExtracted_TestData.csv",
     skip_header=1, delimiter=',')
@@ -36,33 +35,31 @@ writer = csv.writer(f)
 writer.writerow(scores)
 f.close()
 
-# j = 1
-# for item in scores:
-#     print ("\n Sample Number",j)
-#     j = j + 1
-#     if item > 3:
-#         print("Movement Fully Done")
-#     elif item > 0 and item < 3:
-#         print("Movement Partially Done")
-#     else :
-#         print("Movement Is Not Done")
+j = 1
+for item in scores:
+    print("\n Sample Number", j)
+    j = j + 1
+    if item > 3:
+        print("Movement Fully Done")
+    elif item > 0 and item < 3:
+        print("Movement Partially Done")
+    else:
+        print("Movement Is Not Done")
 
+print("\nPredection Scores:\n", pred)
+j = 0
+print("\nOutlined samples are:")
+for item in pred:
+    j = j + 1
+    if (item == -1):
+        print(j, end=", ")
 
-# print("\nPredection Scores:\n",pred)
-# j = 0
-# print ("\nOutlined samples are:")
-# for item in pred:
-#     j = j + 1
-#     if (item == -1):
-#         print(j, end =", ")
-
-
-# thresh = quantile(scores, 0.03)
-# print("\n Threshold is \n ",thresh)
+thresh = quantile(scores, 0.03)
+print("\n Threshold is \n ", thresh)
 
 # anom_index = where(pred==-1)
 # values = x[anom_index]
-
+#
 # for item in anom_index:
 #     print("\n Outlined samples are samples number\n", item)
 
