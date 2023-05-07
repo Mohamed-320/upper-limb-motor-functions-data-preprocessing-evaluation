4cdaaa0fbc5e3bf35c986ca36292d91a9e7a3fb6
Mohamed-320 <muhmmadabdelaziz97@gmail.com>
2023-05-07 01:55:47 +0100

    Created a descision trees algo

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,22 +1,24 @@
 # Import required libraries
+
 import pandas as pd
-from numpy import genfromtxt
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 
-# Load the data
-# data = pd.read_csv('post-stroke-data.csv')
-data = genfromtxt(
-    r"H:\zizo-thesis\upper-limb-motor-functions-data-preprocessing-evaluation\DataPreProcessing\FeaturesExtracted.csv",
-    skip_header=1, delimiter=',')
+# Load the training data and the test data
+data = pd.read_csv(
+    r"H:\zizo-thesis\upper-limb-motor-functions-data-preprocessing-evaluation\DataPreProcessing\FeaturesExtracted.csv")
+fmaScores = pd.read_csv(
+    r"H:\zizo-thesis\upper-limb-motor-functions-data-preprocessing-evaluation\DataPreProcessing\Results.csv")
+# print("=======THE TRAINED DATA FOR THE Decision TREE ALGORITHM=======" + trainData)
 
 # Preprocess the data
-# Create a dataset with only one class of data points
-# new_df = pd.DataFrame(StandardScaler().fit_transform(df), columns=df.columns, index=df.index)
-
-X = data.drop([1, 2, 3, 4], axis=1)
-y = data[1, 2, 3, 4]
+X = data.dropna(axis=1)
+print("the X data includes the kinematic measures for each patient in the preprocessed dataset")
+print(X)
+y = data['FMAScore']  # fix  Y value issue it should be the targeted value
+print("The corresponding Fugl-Meyer scores for each patient in the preprocessed dataset")
+print(y)
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -27,6 +29,7 @@ model.fit(X_train, y_train)
 
 # Make predictions on the test set
 y_pred = model.predict(X_test)
+print(y_pred)
 
 # Evaluate the model's performance
 mse = mean_squared_error(y_test, y_pred)
