85047ed808598e6c09e32573ab619defc8e5261c
Mohamed-320 <muhmmadabdelaziz97@gmail.com>
2023-11-12 03:00:49 +0200

    Added Random Forest Algorithm

--- RandomForest.py
+++ RandomForest.py
@@ -37,53 +37,53 @@ for fold, score in enumerate(scores, start=1):
 average_accuracy = np.mean(1 - scores)
 print(f"Average Accuracy: {average_accuracy:.2f}")
 
-# ## For the upper extremity assessment (which assesses functions like shoulder, elbow, forearm, wrist, and hand movements), the score typically ranges from 0 to 66.
-#
-# i = 1
-# for item in y:
-#     print("\n Sample", i, "Score is ", item, end=" ")
-#     i = i + 1
-#
-# results = open(r"H:\zizo-thesis\upper-limb-motor-functions-data-preprocessing-evaluation\datapreprocessing\Results.csv",
-#                'a', newline='')
-# writer = csv.writer(results)
-# writer.writerow(scores)
-# results.close()
-#
-# j = 1
-# for item in scores:
-#     print("\n Sample Number", j)
-#     j = j + 1
-#     if item > 3:
-#         print("Movement Fully Done")
-#     elif item > 0 and item < 3:
-#         print("Movement Partially Done")
-#     else:
-#         print("No Movement")
-#
-# # Convert predicted_labels to 1D array for anchor usage
-# predicted_labels = np.squeeze(scores)
-#
-#
-# # Create a function that returns True if the predicted label matches the actual label
-# def predict_fn(x):
-#     return scores[x]
-#
-#
-# # Initialize the Anchor explainer
-# explainer = anchor_tabular.AnchorTabularExplainer(
-#     X,  # Training data
-#     data.columns.tolist(),  # Feature names
-#     predict_fn,  # Prediction function
-#     categorical_names=None  # Categorical feature names, if applicable
-# )
-#
-# # Choose a data point to explain (replace 'index' with the desired index)
-# index = 0
-# explanation = explainer.explain_instance(X.iloc[index].values)
-#
-# # Print the anchor rule explanation
-# print('Explanation for prediction:')
-# print('Anchor Rule for Explanation: %s' % (' AND '.join(explanation.names())))
-# print('Precision: %.2f' % explanation.precision())
-# print('Coverage: %.2f' % explanation.coverage())
+## For the upper extremity assessment (which assesses functions like shoulder, elbow, forearm, wrist, and hand movements), the score typically ranges from 0 to 66.
+
+i = 1
+for item in y:
+    print("\n Sample", i, "Score is ", item, end=" ")
+    i = i + 1
+
+results = open(r"H:\zizo-thesis\upper-limb-motor-functions-data-preprocessing-evaluation\datapreprocessing\Results.csv",
+               'a', newline='')
+writer = csv.writer(results)
+writer.writerow(scores)
+results.close()
+
+j = 1
+for item in scores:
+    print("\n Sample Number", j)
+    j = j + 1
+    if item > 3:
+        print("Movement Fully Done")
+    elif item > 0 and item < 3:
+        print("Movement Partially Done")
+    else:
+        print("No Movement")
+
+# Convert predicted_labels to 1D array for anchor usage
+predicted_labels = np.squeeze(scores)
+
+
+# Create a function that returns True if the predicted label matches the actual label
+def predict_fn(x):
+    return scores[x]
+
+
+# Initialize the Anchor explainer
+explainer = anchor_tabular.AnchorTabularExplainer(
+    X,  # Training data
+    data.columns.tolist(),  # Feature names
+    predict_fn,  # Prediction function
+    categorical_names=None  # Categorical feature names, if applicable
+)
+
+# Choose a data point to explain (replace 'index' with the desired index)
+index = 0
+explanation = explainer.explain_instance(X.iloc[index].values)
+
+# Print the anchor rule explanation
+print('Explanation for prediction:')
+print('Anchor Rule for Explanation: %s' % (' AND '.join(explanation.names())))
+print('Precision: %.2f' % explanation.precision())
+print('Coverage: %.2f' % explanation.coverage())
